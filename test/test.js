ec29ec14c96f55b1d443c6622597bf53135fd1ce
kkeeth <zensin0082@gmail.com>
2026-01-07 16:31:35 +0900

    chore: goodbye ava

--- test/test.js
+++ test/test.js
@@ -1,22 +1,25 @@
-import test from 'ava';
-import pify from 'pify';
+import { test } from 'node:test';
+import assert from 'node:assert';
+import { execFile } from 'node:child_process';
+import { promisify } from 'node:util';
 import * as greetings from '../lib/greetings.js';
-import { execFile } from 'child_process';
 
-test('select one option', (t) => {
+const execFilePromise = promisify(execFile);
+
+test('select one option', () => {
   // normal case
-  t.truthy(greetings.get('new'));
+  assert.ok(greetings.get('new'));
 
   // error check
-  t.false(greetings.get());
-  t.false(greetings.get('all'));
-  t.false(greetings.get('help'));
-  t.false(greetings.get('l'));
-  t.false(greetings.get('hoge'));
-  t.is(greetings.replaceEx('hoge'), 'Cannot parse greeting file');
+  assert.strictEqual(greetings.get(), false);
+  assert.strictEqual(greetings.get('all'), false);
+  assert.strictEqual(greetings.get('help'), false);
+  assert.strictEqual(greetings.get('l'), false);
+  assert.strictEqual(greetings.get('hoge'), false);
+  assert.strictEqual(greetings.replaceEx('hoge'), 'Cannot parse greeting file');
 });
 
-test('select multi options', (t) => {
+test('select multi options', () => {
   const opts = [
     'new',
     'last',
@@ -29,10 +32,10 @@ test('select multi options', (t) => {
   ];
 
   // error check
-  t.false(greetings.get(opts));
+  assert.strictEqual(greetings.get(opts), false);
 });
 
-test('check greeting list', (t) => {
+test('check greeting list', () => {
   const list = greetings.getGreetings();
   let list_length = 0;
   for (let key in list) {
@@ -40,86 +43,111 @@ test('check greeting list', (t) => {
   }
 
   // error check
-  t.is(list_length, 7);
-  t.true('a' in list);
-  t.true('n' in list);
-  t.true('s' in list);
-  t.true('w' in list);
-  t.true('t' in list);
-  t.true('so' in list);
-  t.false('z' in list);
+  assert.strictEqual(list_length, 7);
+  assert.ok('a' in list);
+  assert.ok('n' in list);
+  assert.ok('s' in list);
+  assert.ok('w' in list);
+  assert.ok('t' in list);
+  assert.ok('so' in list);
+  assert.strictEqual('z' in list, false);
 });
 
-test('check dialect list', (t) => {
+test('check dialect list', () => {
   const list = greetings.getDialects();
 
   // error check
-  t.is(list.length, 4);
-  t.true(list.indexOf('kyoto') >= 0);
-  t.true(list.indexOf('osaka') >= 0);
-  t.true(list.indexOf('okinawa') >= 0);
-  t.true(list.indexOf('hiroshima') >= 0);
-  t.false(list.indexOf('hokkaido') >= 0);
+  assert.strictEqual(list.length, 4);
+  assert.ok(list.indexOf('kyoto') >= 0);
+  assert.ok(list.indexOf('osaka') >= 0);
+  assert.ok(list.indexOf('okinawa') >= 0);
+  assert.ok(list.indexOf('hiroshima') >= 0);
+  assert.strictEqual(list.indexOf('hokkaido') >= 0, false);
 });
 
-test('check language list', (t) => {
+test('check language list', () => {
   const list = greetings.getLanguages();
 
   // error check
-  t.is(list.length, 2);
-  t.true(list.indexOf('en') >= 0);
-  t.true(list.indexOf('ch') >= 0);
-  t.false(list.indexOf('fr') >= 0);
+  assert.strictEqual(list.length, 2);
+  assert.ok(list.indexOf('en') >= 0);
+  assert.ok(list.indexOf('ch') >= 0);
+  assert.strictEqual(list.indexOf('fr') >= 0, false);
 });
 
-test('check surround list', (t) => {
+test('check surround list', () => {
   const list = greetings.getSurrounds();
 
   // error check
-  t.is(list.length, 7);
-  t.true(list.indexOf('no') >= 0);
-  t.true(list.indexOf('w-star') >= 0);
-  t.true(list.indexOf('b-star') >= 0);
-  t.true(list.indexOf('asterisk') >= 0);
-  t.true(list.indexOf('w-tri') >= 0);
-  t.true(list.indexOf('b-tri') >= 0);
-  t.true(list.indexOf('slash') >= 0);
-  t.false(list.indexOf('hoge') >= 0);
+  assert.strictEqual(list.length, 7);
+  assert.ok(list.indexOf('no') >= 0);
+  assert.ok(list.indexOf('w-star') >= 0);
+  assert.ok(list.indexOf('b-star') >= 0);
+  assert.ok(list.indexOf('asterisk') >= 0);
+  assert.ok(list.indexOf('w-tri') >= 0);
+  assert.ok(list.indexOf('b-tri') >= 0);
+  assert.ok(list.indexOf('slash') >= 0);
+  assert.strictEqual(list.indexOf('hoge') >= 0, false);
 });
 
-test('surround exist check', async (t) => {
+test('surround exist check', async () => {
   // error
-  let stdout = await pify(execFile)('./bin/jgr', ['last']);
-  t.notRegex(stdout, /#+/);
+  let result = await execFilePromise('./bin/jgr', ['last']);
+  let stdout = result.stdout;
+  assert.doesNotMatch(stdout, /#+/);
 
-  // default
-  t.regex(stdout, /-+/);
+  // default (Japanese uses full-width hyphen)
+  assert.match(stdout, /ー+/);
 
   // b-star
-  stdout = await pify(execFile)('./bin/jgr', ['-s', 'b-star', 'last']);
-  t.regex(stdout, /★+/);
+  result = await execFilePromise('./bin/jgr', ['-s', 'b-star', 'last']);
+  stdout = result.stdout;
+  assert.match(stdout, /★+/);
 
   // slash
-  stdout = await pify(execFile)('./bin/jgr', ['-s', 'slash', 'last']);
-  t.regex(stdout, /\/￣/);
+  result = await execFilePromise('./bin/jgr', ['-s', 'slash', 'last']);
+  stdout = result.stdout;
+  assert.match(stdout, /\/￣/);
 
   // no surround
-  stdout = await pify(execFile)('./bin/jgr', ['-s', 'no', 'last']);
-  stdout = stdout.split(/\r\n|\r|\n/)[1];
-  t.is(stdout, '　　　　　本');
+  result = await execFilePromise('./bin/jgr', ['-s', 'no', 'last']);
+  stdout = result.stdout.split(/\r\n|\r|\n/)[1];
+  assert.strictEqual(stdout, '　　　　　本');
 
   // null check
-  stdout = await pify(execFile)('./bin/jgr', ['-s', 'last']);
-  t.is(stdout, '');
+  result = await execFilePromise('./bin/jgr', ['-s', 'last']);
+  stdout = result.stdout;
+  assert.strictEqual(stdout, '');
 });
 
-test('dialect exist check', async (t) => {
+test('dialect exist check', async () => {
   // okinawa
-  let stdout = await pify(execFile)('./bin/jgr', ['-d', 'okinawa', 'last']);
-  stdout = stdout.split(/\r\n|\r|\n/)[5];
-  t.is(stdout, '　ん　話で');
+  let result = await execFilePromise('./bin/jgr', ['-d', 'okinawa', 'last']);
+  let stdout = result.stdout.split(/\r\n|\r|\n/)[5];
+  assert.strictEqual(stdout, '　ん　話で');
 
   // null check
-  stdout = await pify(execFile)('./bin/jgr', ['-d', 'last']);
-  t.is(stdout, '');
+  result = await execFilePromise('./bin/jgr', ['-d', 'last']);
+  stdout = result.stdout;
+  assert.strictEqual(stdout, '');
+});
+
+test('default hyphen changes based on language', async () => {
+  // Japanese (default) - should use full-width hyphen (ー)
+  let result = await execFilePromise('./bin/jgr', ['last']);
+  let stdout = result.stdout;
+  assert.match(stdout, /ー+/);
+  assert.doesNotMatch(stdout, /^-+$/m);
+
+  // English - should use half-width hyphen (-)
+  result = await execFilePromise('./bin/jgr', ['-l', 'en', 'last']);
+  stdout = result.stdout;
+  assert.match(stdout, /-+/);
+  assert.doesNotMatch(stdout, /ー+/);
+
+  // Chinese - should use half-width hyphen (-)
+  result = await execFilePromise('./bin/jgr', ['-l', 'ch', 'last']);
+  stdout = result.stdout;
+  assert.match(stdout, /-+/);
+  assert.doesNotMatch(stdout, /ー+/);
 });
